e1517cdc17c3524773ac5ba382f60f5d7fa02ac7
dedonmonday <dedonmonday@users.noreply.github.com>
2018-03-26 16:49:32 -0400

    Add files via upload

---
+++ Bank/Bank/Bank/Core/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Bank.Core {
+    internal class Transaction {
+        public DateTime TransactionDate { get; set; }
+        public string TransactionMemo { get; set; }
+        public double TransactionAmount { get; set; }
+        public bool TransactionDestination { get; set; }
+
+        public static Transaction CreateTransaction(string transactionMemo, double amount, bool sending) {
+            return new Transaction {
+                TransactionDate = DateTime.Now,
+                TransactionMemo = transactionMemo,
+                TransactionAmount = amount,
+                TransactionDestination = sending
+            };
+        }
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Bank.Core;
+using Bank.Bank.Core.Accounts;
+
+namespace Bank {
+    internal class Program {
+        static void Main(string[] args) {
+            BankManager bankManager = new BankManager();
+            Customer c = Customer.CreateCustomer("A", "adrin", "1003");
+            Account a = Account.CreateAccount(c, "10100101", 100, 0 , 0);
+            a.AccountTransactions.Add(Transaction.CreateTransaction("no", 100000, false));
+
+            Console.WriteLine(bankManager.Validatetransactions(a));
+            Console.ReadLine();
+        }
+    }
+}
